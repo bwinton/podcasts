f63c8e5c2e743e1d482722ceb2f8aeb5392fcecc
Blake Winton <bwinton@latte.ca>
2021-07-25 20:06:34 -0400

    Add prettier tracebacks.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate podcasts;
 extern crate rayon;
 
+use color_eyre::Report;
 use rayon::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Report> {
+    color_eyre::install()?;
+
     let names = vec!["spodcast", "diecast", "vortex_theatre"];
     names.par_iter().for_each(|name| podcasts::handle(name));
+    Ok(())
 }
